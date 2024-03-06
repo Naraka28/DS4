64e38de4185885f5cac47c45e66e154e851c7027
Naraka28 <eildan28@hotmail.com>
2024-03-05 17:47:43 -0700

    ya quedó alv

--- scrapper.py
+++ scrapper.py
@@ -30,42 +30,37 @@ def get_exchange_rate(dom):
         value=value.text.strip()
         exchange_rate[title]=value
     return exchange_rate
-def get_institutions():
-    url="https://bit.ly/dolarInfo"
-    pagina=scrap(url)
-    soup=BeautifulSoup(pagina.content,"html.parser")
-    result=soup.find(id="dllsTable")
-    dic=get_exchange_rate_dict(result)
-    print(dic)
-
-
 def get_exchange_rate_dict(dom):
-    inst={}
     body=dom.find("tbody")
+    inst={}
     for row in body.find_all("tr"):
         i=0
         long=len(row.find_all("td"))
         for col in row.find_all("td"):
-            if long<4:
-                if i==0:
-                    institucion=col.find(class_="small-hide")
-                    institucion=institucion.text.strip()
-                if i==2:
-                    compra=float(compra.text.strip())
-                    print(compra)
-            else:
+            print(f"{col} {i}")
+            if long<5:
                 if i==0:
                     institucion=col.find(class_="small-hide")
                     institucion=institucion.text.strip()
                     print(institucion)
-                if i==2:
-                    compra=float(compra.text.strip())
-                    print(compra)
                 if i==3:
-                    venta=float(venta.text.strip())
+                    compra=float(col.text.strip())
+                    print(compra)
+            else:
+                if i==0:
+                    print(f"{col} {i}")
+                    institucion=col.find(class_="small-hide")
+                    institucion=institucion.text.strip()
+                    print(institucion)
+                if i==3:
+                    compra=float(col.text.strip())
+                    print(compra)
+                if i==4:
+                    venta=float(col.text.strip())
                     print(venta)
-        i+=1
-    inst[institucion]={"Compra":compra,"Venta":venta}
+            i+=1
+        inst[institucion]=f"Compra: {compra} Venta: {venta}"
+    return inst
 
 
 
