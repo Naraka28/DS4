8509ec11cc19b8f39170d1565ca49fd1c8b57385
Naraka28 <eildan28@hotmail.com>
2024-03-09 14:25:35 -0700

    se implementó FIX

--- scrapper.py
+++ scrapper.py
@@ -14,8 +14,16 @@ def main():
     # result=soup.find(class_="exchangeRate")
     # ex=get_exchange_rate(result)
     result=soup.find(id="dllsTable")
-    dic=get_exchange_rate_dict(result)
-    print(dic)
+    dic_list=get_exchange_rate_dict(result)
+    fix_dic=dic_list.pop()
+    bank_dic=dic_list.pop()
+    print(bank_dic)
+    print(fix_dic)
+    maximo_dolar(bank_dic,"Compra")
+    min_dolar(bank_dic,"Compra")
+    maximo_dolar(bank_dic,"Venta")
+    min_dolar(fix_dic,"FIX")
+    maximo_dolar(fix_dic,"FIX")
 
 def get_exchange_rate(dom):
     exchange_rate={}
@@ -33,6 +41,7 @@ def get_exchange_rate(dom):
 def get_exchange_rate_dict(dom):
     body=dom.find("tbody")
     inst={}
+    fix_dic={}
     for row in body.find_all("tr"):
         i=0
         long=len(row.find_all("td"))
@@ -41,24 +50,39 @@ def get_exchange_rate_dict(dom):
                 if i==0:
                     institucion=col.find(class_="small-hide")
                     institucion=institucion.text.strip()
-                    print(institucion)
                 if i==3:
                     compra=float(col.text.strip())
-                    print(compra)
+                    fix_dic[institucion]={"FIX":compra}
             else:
                 if i==0:
                     institucion=col.find(class_="small-hide")
                     institucion=institucion.text.strip()
-                    print(institucion)
                 if i==3:
                     compra=float(col.text.strip())
-                    print(compra)
                 if i==4:
                     venta=float(col.text.strip())
-                    print(venta)
+                    inst[institucion]={"Compra":compra, "Venta": venta}
             i+=1
-        inst[institucion]=f"Compra: {compra} Venta: {venta}"
-    return inst
+    list_dic=[inst,fix_dic]
+    return list_dic
+def maximo_dolar(dic:dict,eleccion:str):
+    compra_max=0
+    for k in dic:
+       compra=dic[k][eleccion]
+       if compra_max<compra:
+           banco=k
+           compra_max=compra
+    print(f"Banco: {banco} {eleccion}_max: {compra_max}")
+def min_dolar(dic:dict,eleccion:str):
+    compra_min=1000
+    for k in dic:
+       compra=dic[k][eleccion]
+       if compra<compra_min:
+           banco=k
+           compra_min=compra
+    print(f"Banco: {banco} {eleccion}_min: {compra_min}")
+
+
 
 
 
